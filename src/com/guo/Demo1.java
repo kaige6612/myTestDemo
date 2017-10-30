9bb2b7f60ffae061b43f2e4fbe06f39733a96edc
guoqingkai <guoqingkai@example.com>
2017-10-30 10:26:54 +0800

    Initial commit  将整个项目上传至github

---
+++ src/com/guo/Demo1.java
@@ -0,0 +1,9 @@
+package com.guo;
+
+public class Demo1 {
+   public void sayHi() {
+       for(int num=0;num<5;num++) {
+           System.out.println("第" + num + "次打印");
+       }
+   }
+}
---
+++ src/com/guo/Demo101.java
@@ -0,0 +1,8 @@
+package com.guo;
+
+public class Demo101 extends Demo1 {
+    @Override
+    public void sayHi() {
+        System.out.println("你好");
+    }
+}
