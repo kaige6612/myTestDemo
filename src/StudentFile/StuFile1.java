75541440a0804c5a2e6b8f853118c4e0ed8bbb22
guoqingkai <guoqingkai@example.com>
2017-10-30 11:54:37 +0800

    增加跑的方法

--- src/StudentFile/StuFile1.java
+++ src/StudentFile/StuFile1.java
@@ -4,4 +4,9 @@ public class StuFile1 {
     public void  eat() {
         System.out.println("动物的共性");
     }
+
+    //增加跑的方法
+    public void run(){
+        System.out.println("just run");
+    }
 }
