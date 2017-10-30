8263beca37d9075d5ce442356d517208a36e3ea4
guoqingkai <guoqingkai@example.com>
2017-10-30 11:56:23 +0800

    增加跑的方法

--- src/StudentFile/StuFile1.java
+++ src/StudentFile/StuFile1.java
@@ -4,4 +4,9 @@ public class StuFile1 {
     public void  eat() {
         System.out.println("动物的共性");
     }
+
+    //增加飞的方法
+    public void fly() {
+        System.out.println("bird can fly,but I can not.");
+    }
 }
