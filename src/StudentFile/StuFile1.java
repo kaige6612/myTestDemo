e59da6f3cb961d1344ba04ceca5a7c520bd1754a
guoqingkai <guoqingkai@example.com>
2017-10-30 11:59:38 +0800

    增加fei 的方法

--- src/StudentFile/StuFile1.java
+++ src/StudentFile/StuFile1.java
@@ -9,4 +9,9 @@ public class StuFile1 {
     public void run(){
         System.out.println("just run");
     }
+
+    //增加飞的方法
+    public void fly() {
+        System.out.println("bird can fly,but I can not.");
+    }
 }
